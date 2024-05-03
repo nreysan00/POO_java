10d97ac47369c6fdfa78fdb649940f222319cc34
nreysan00 <3dnatanreyessanchez@gmail.com>
2024-05-03 14:24:36 +0200

    correcion Agenda Telefonica

--- ejercicio8/src/ejercicio8/agenda.java
+++ ejercicio8/src/ejercicio8/agenda.java
@@ -1,136 +1,164 @@
 package ejercicio8;
 
 public class agenda {
-	
-	//Atributos
-	private contacto[] contactos;
-	
-	//Constructores
-	public agenda() {
-		this.contactos = new contacto[10];	//Por defecto
-	}
-	
-	public agenda(int tamanio) {
-		this.contactos = new contacto[tamanio];	//Tamaño que nosotros queramos
-	}
-	
-	//Métodos
-	/**
-	 * Añade un contacto a la agenda
-	 */
-	public void aniadirContacto(contacto c) {
-		
-		if (existeContacto(c)) {	//Indico si existe el contacto
-			System.out.println("El contacto con ese nombre ya existe");
-		}else if (agendaLlena()) {	//Indico si la agenda está llena o no llena
-			System.out.println("La agenda está llena, no se pueden meter mas contactos");
-		}else {
-			
-			boolean encontrado = false;
-			for(int i=0; i < contactos.length && !encontrado; i++) {
-				if (contactos[i] == null) {	//Controlo los nulos
-					contactos[i] = c;	//Inserto el contacto
-					encontrado = true;	//Indico que lo he encontrado
-				}
-			}
-			if (encontrado) {
-				System.out.println("Se ha añadido");
-			}else {
-				System.out.println("No se ha podido añadir");
-			}
-		}
-	}
-	
-	/**
-	 * Indica si existe un contacto
-	 */
-	public boolean existeContacto(contacto c) {
-		
-		for (int i=0; i < contactos.length; i++) {
-			//Controlo nulos e indico si el contacto es el mismo
-			if (contactos[i] != null && c.equals(contactos[i])) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * Lista los contactos de la agenda
-	 */
-	public void listarContactos() {
-		if (huecosLibre() == contactos.length) {
-			System.out.println("No hay contanctos que mostrar");
-		}else {
-			for (int i = 0; i < contactos.length; i++) {
-				if (contactos[i] != null) {	//Controlo nulo
-					System.out.println(contactos[i]);
-				}
-			}
-		}
-	}
-	
-	/**
-	 * Busca un contacto por su nombre
-	 */
-	public void buscarPorNombre(String nombre) {
-		
-		boolean encontrado = false;
-		for (int i=0; i < contactos.length && !encontrado; i++) {
-			//Controlo nulos y compruebo que es el contacto buscado (forma mas engorrosa)
-			if (contactos[i] != null && contactos[i].getNombre().trim().equalsIgnoreCase(nombre.trim())) {
-				System.out.println("Su telefono es " + contactos[i].getTelefono());
-				encontrado = true;	//Indico que lo he encontrado
-			}
-		}
-		if (!encontrado) {
-			System.out.println("No se ha encontrado el contacto");
-		}
-	}
-	
-	/**
-	 * Indica si la agenda esta llena o no
-	 */
-	public boolean agendaLlena() {
-		for (int i=0; i < contactos.length; i++) {
-			if (contactos[i] == null) {	//Controlo nulos
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	/**
-	 * Elimina un contacto de la agenda
-	 */
-	public void eliminarContacto(contacto c) {
-		
-		boolean encontrado = false;
-		for (int i=0; i < contactos.length; i++) {
-			if (contactos[i] != null && contactos[i].equals(c)) {
-				contactos[i] = null;	//Controlo nulos
-				encontrado = true;	//Indico que lo he encontrado
-			}
-		}
-		
-		if (encontrado) {
-			System.out.println("Se ha eliminado el contacto");
-		}else {
-			System.out.println("No se ha eliminado el contacto");
-		}
-	}
-	
-	/**
-	 * Indica cuantos contactos más podemos meter
-	 */
-	public int huecosLibre() {
-		
-		int contadorLibres = 0;
-		for (int i=0; i < contactos.length; i++) {
-			if (contactos[i] == null) {	//Controlo nulos
-				contadorLibres++;	//Acumulo
-			}
-		}
-		return contadorLibres;
-	}
+	 
+    //Atributos
+    private contacto[] contactos;
+ 
+    //Constructores
+    public agenda() {
+        this.contactos = new contacto[10]; //por defecto
+    }
+ 
+    public agenda(int tamanio) {
+        this.contactos = new contacto[tamanio]; //tamaño que nosotros queramos
+    }
+ 
+    //Metodos
+    /**
+     * Añade un contacto a la agenda
+     *
+     * @param c
+     */
+    public void aniadirContacto(contacto c) {
+ 
+        if (existeContacto(c)) { //Indico si existe el contacto
+            System.out.println("El contacto con ese nombre ya existe");
+        } else if (agendaLlena()) { //Indico si la agenda esta o no llena
+            System.out.println("La agenda esta llena, no se pueden meter mas contactos");
+        } else {
+ 
+            boolean encontrado = false;
+            for (int i = 0; i < contactos.length && !encontrado; i++) {
+                if (contactos[i] == null) { //controlo los nulos
+                    contactos[i] = c; //Inserto el contacto 
+                    encontrado = true; //Indico que lo he encontrado
+                }
+            }
+ 
+            if (encontrado) {
+                System.out.println("Se ha añadido");
+            } else {
+                System.out.println("No se ha podido añadir");
+            }
+        }
+ 
+    }
+ 
+    /**
+     * Indica si existe un contacto
+     *
+     * @param c
+     * @return
+     */
+    public boolean existeContacto(contacto c) {
+ 
+        for (int i = 0; i < contactos.length; i++) {
+            //Controlo nulos e indico si el contacto es el mismo
+            if (contactos[i] != null && c.equals(contactos[i])) {
+                return true;
+            }
+        }
+        return false;
+ 
+    }
+ 
+    /**
+     * Lista los contactos de la agenda
+     */
+    public void listarContactos() {
+ 
+        if (huecosLibre() == contactos.length) {
+            System.out.println("No hay contactos que mostrar");
+        } else {
+            for (int i = 0; i < contactos.length; i++) {
+                if (contactos[i] != null) { //Controlo nulos
+                    System.out.println(contactos[i]);
+                }
+            }
+        }
+ 
+    }
+ 
+    /**
+     * Busca un contacto por su nombre
+     *
+     * @param nombre
+     */
+    public void buscarPorNombre(String nombre) {
+ 
+        boolean encontrado = false;
+        for (int i = 0; i < contactos.length && !encontrado; i++) {
+            //Controlo nulos y cxompruebo que es el contacto buscado (forma mas engorrosa)
+            if (contactos[i] != null && contactos[i].getNombre().trim().equalsIgnoreCase(nombre.trim())) {
+                System.out.println("Su telefono es " + contactos[i].getTelefono()); //muestro el telefono
+                encontrado = true; //Indico que lo he encontrado
+            }
+        }
+ 
+        if (!encontrado) {
+            System.out.println("No se ha encontrado el contacto");
+        }
+ 
+    }
+ 
+    /**
+     * Indica si la agenda esta llena o no
+     *
+     * @return
+     */
+    public boolean agendaLlena() {
+ 
+        for (int i = 0; i < contactos.length; i++) {
+            if (contactos[i] == null) { //Controlo nulos
+                return false; //indico que no esta llena
+            }
+        }
+ 
+        return true; //esta llena
+ 
+    }
+ 
+    /**
+     * Elimina el contacto de la agenda
+     *
+     * @param c
+     */
+    public void eliminarContacto(contacto c) {
+ 
+        boolean encontrado = false;
+        for (int i = 0; i < contactos.length && !encontrado; i++) {
+            if (contactos[i] != null && contactos[i].equals(c)) {
+                contactos[i] = null; //Controlo nulos
+                encontrado = true; //Indico que lo he encontrado
+            }
+        }
+ 
+        if (encontrado) {
+            System.out.println("Se ha eliminado el contacto");
+        } else {
+            System.out.println("No se ha eliminado el contacto");
+ 
+        }
+ 
+    }
+ 
+    /**
+     * Indica cuantos contactos más podemos meter
+     *
+     * @return
+     */
+    public int huecosLibre() {
+ 
+        int contadorLibres = 0;
+        for (int i = 0; i < contactos.length; i++) {
+            if (contactos[i] == null) { //Controlo nulos
+                contadorLibres++; //Acumulo
+            }
+        }
+ 
+        return contadorLibres;
+ 
+    }
+ 
 }
